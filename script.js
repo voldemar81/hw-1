07a56d5b28e86deab634baac51099357652c925b
Vladimir U <guru81@mail.ru>
2023-03-26 22:56:33 +0300

    вёрстка по 2 дз +зaдачи  js

---
+++ script.js
@@ -0,0 +1,78 @@
+/*
+Задача 1
+let a = 20;
+alert(a);
+*/
+
+/*
+а = дата создания 1-го iPhone
+let a = 2007;
+alert(a);
+*/
+
+/*
+a = имя создателя JS
+let a = 'Брендан Эйх';
+alert(a);
+*/
+
+/*
+задача 4
+let a = 10;
+let b = 2;
+alert(a+b); 
+alert(a-b);
+alert(a*b);
+alert(a/b);
+*/
+
+/*
+задача 5
+let result = (2**5);
+alert(result);
+*/
+
+/*
+задача 6
+let a = 9;
+let b =2;
+alert(9%2);
+*/
+
+/*
+задача 7
+let num = 1;
+num +=5;
+num -=3;
+num *=7;
+num /=3
+num++;
+num--;
+alert(num);
+*/
+
+/*
+задача 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+*/
+
+/*
+const user = {
+    name: 'Jon',
+    age: 30, 
+	isAdmin: false 
+}
+
+user['city of residence'] = true
+delete user.age
+user.age = 35
+delete ['city of residence']
+
+
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[info]);
+
+let name = prompt('Назовите ваше имя')
+alert(`Привет,${name}`);
+*/
