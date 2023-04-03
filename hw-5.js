b10896e4b27936a5cb2d1e2588db4c1741882709
Vladimir U <guru81@mail.ru>
2023-04-03 23:44:38 +0300

    Начата работа над задачами 5  го урока

---
+++ hw-5.js
@@ -0,0 +1,34 @@
+function minNumber(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+  // mission 1
+
+  function evenOddNumber(c) {
+    if (c % 2 === 0 ) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+      }
+  }
+   // mission 2
+
+   function evenOddNumber(c) {
+    return (c % 2 === 0 ? 'Число четное' : 'Число  нечетное')
+}
+ // mission 2
+
+ let square = function (d) {
+    return (d * d)
+   
+ }
+ console.log(square(10))
+
+ function numberSquare(d) {
+    return (d * d)
+
+ }
+ // mission 3
