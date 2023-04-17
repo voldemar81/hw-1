404c5d13394c7128a559352b3c4f9c3e5bdbd81d
Vladimir U <guru81@mail.ru>
2023-04-17 23:43:57 +0300

    ready

--- hw-8.js
+++ hw-8.js
@@ -1,4 +1,4 @@
-function mult(a,b) {
+ function mult(a,b) {
     return a*b;
    }
    
@@ -10,13 +10,13 @@ function mult(a,b) {
    const result = arr.reduce((resultValue,currentValue)=> {
        const currentResult = callback (resultValue,currentValue)
        return currentResult;
-   })
+   });
      return result;
    }
    
    const arr = [3, 4, 1, 9];
    
-   console.log(getResult(arr,sum)); console.log(findWord(animals,'ко'));
+   console.log(getResult(arr,sum)); 
    console.log(getResult(arr,mult));
    // mission 1
    
@@ -36,3 +36,55 @@ function mult(a,b) {
      getResult(sum, arr);
      //mission 1.1
    
+    const users = [
+        {name: 'Jon', age: 22},
+        {name: 'Richard', age: 18},
+        {name: 'Anton', age: 32},
+        {name: 'Lida', age: 23},
+        {name: 'Bob', age: 44}
+    ]; 
+    
+     
+  const arr = users.sort((a,b) => a.age - b.age);
+  console.log(arr);
+  // mission 2
+
+  function reversArr(arr) {
+    arr.reverse();
+    console.log(arr);
+  }
+  function NumberArr(arr) {
+    arr = arr.filter(item => !isNaN(item));
+    arr = arr.map(Number);
+    console.log(arr);
+  }
+  function each(arr, callback) {
+    callback(arr);
+  }
+  const arr = [1, '4', 9, 'two'];
+  each(arr, reversArr);
+  
+  const arr = [1, '4', false, 9, 'two'];
+  each(arr, NumberArr);
+  //mission 3
+
+ let timer = setInterval(() => console.log(new Date()), 3000);
+ setTimeout(() => { clearInterval(timer); console.log('30 секунд прошло'); }, 30000);
+ //mission 4
+
+  function calling() {
+    console.log('Звоню!')
+  };
+  function beeps(callback) {
+    setTimeout(() => {
+        console.log('Идут гудки...')
+        callback();
+    }, 1000);
+    
+  }
+  function talk() {
+    console.log('Разговор')
+  }
+  calling();
+  beeps(talk);
+ // mission 5
